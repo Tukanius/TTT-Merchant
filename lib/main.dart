f553de12c6dbace9d6796668c0db13ffb6cd5119
Tukanius <79844494+Tukanius@users.noreply.github.com>
2025-09-19 14:12:14 +0800

    main

--- lib/main.dart
+++ lib/main.dart
@@ -110,12 +110,7 @@ class _MyAppState extends State<MyApp> with AfterLayoutMixin {
       child: MaterialApp(
         navigatorKey: NavigationService.navigatorKey,
         builder: (context, child) {
-          return MediaQuery(
-            data: MediaQuery.of(
-              context,
-            ).copyWith(textScaler: const TextScaler.linear(1.0)),
-            child: child!,
-          );
+          return MediaQuery.withNoTextScaling(child: child ?? const SizedBox());
         },
         title: 'TTT Борлуулагч',
         theme: ThemeData(useMaterial3: false),
