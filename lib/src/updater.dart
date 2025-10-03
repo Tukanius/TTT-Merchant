20d8b7a774660b91f95160b9a668282da7339108
Tukanius <79844494+Tukanius@users.noreply.github.com>
2025-10-03 18:16:49 +0800

    updater

--- lib/src/updater.dart
+++ lib/src/updater.dart
@@ -38,7 +38,7 @@ class _UpdaterComponentState extends State<UpdaterComponent>
         debugLogging: true,
         messages: UpgraderMessages(code: 'mn'),
         countryCode: 'MN',
-        minAppVersion: '1.0.11',
+        minAppVersion: '1.0.12',
       ),
       child: widget.child,
     );
